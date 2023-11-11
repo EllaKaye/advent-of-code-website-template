ff3cd151849bb37e8900c4873c460b056023cd0e
EllaKaye <ella.kaye@gmail.com>
2023-11-11 21:49:32 +0000

    Site pretty much ready to go

---
+++ post-template/script.R
@@ -0,0 +1,17 @@
+library(aochelpers)
+# input <- aoc_input_vector(DD, YYYY) # uncomment at end, once correct on test input
+# also consider aoc_input_data_frame() or aoc_input_matrix(), with view = TRUE
+
+browseURL("https://adventofcode.com/YYYY/day/DD")
+# use datapasta addin to copy in test input from the website
+# triple-click in test input box in the puzzle to select, 
+# then choose appropriate paste format from addin
+# comment out once ready to run on full input
+
+# input <- 
+
+# Part 1 ---------------------------------------------------------------------
+
+
+
+# Part 2 ---------------------------------------------------------------------
