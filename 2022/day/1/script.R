ad02b3ab58749a2e7d2c6b391ec5508de3654b9c
EllaKaye <ella.kaye@gmail.com>
2023-11-15 13:40:49 +0000

    Restructure and fill in some content

---
+++ 2022/day/1/script.R
@@ -0,0 +1,17 @@
+library(aochelpers)
+# input <- aoc_input_vector(1, 2022) # uncomment at end, once correct on test input
+# also consider aoc_input_data_frame() or aoc_input_matrix(), with view = TRUE
+
+browseURL("https://adventofcode.com/2022/day/1")
+# use datapasta addin to copy in test input from the website
+# triple-click in test input box in the puzzle to select, 
+# then choose appropriate paste format from addin
+# comment out once ready to run on full input
+
+# input <- 
+
+# Part 1 ---------------------------------------------------------------------
+
+
+
+# Part 2 ---------------------------------------------------------------------
